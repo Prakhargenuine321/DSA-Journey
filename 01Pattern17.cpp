8b658fff3416d295262b60efdd2330195f9ccaf1
Prakhar <prakhargenuine9336@gmail.com>
2024-01-19 15:16:07 +0530

    This is commit for pattern number 17

--- 01Pattern17.cpp
+++ 01Pattern17.cpp
@@ -3,31 +3,35 @@ using namespace std;
 
 int main(){
     //Numeric Hollow Pyramid
-    int n = 5;
-    for (int row = 0; row < n; row++)
-    {
-        for (int col = 0; col < row +1 ; col++)
+    // 1 
+    // 1 2
+    // 1   3 
+    // 1     4
+    // 1       5
+    // 1         6
+    // 1           7
+    // 1             8
+    // 1 2 3 4 5 6 7 8 9
+    int n = 9;
+    cout << "1 "<<endl;
+    for(int i = 0;i<n-2;i++){
+        for (int j = 0; j <= i+1; j++)
         {
-            if (col == row)
-            {
-                cout << col+1 << " ";
+            if(j==0){
+                cout << "1 ";
+            }
+            else if(j==i+1){
+                cout << i+2 <<" ";
             }
             else{
-                cout << "  "; 
+                cout << "  ";
             }
         }
-        if (row == n-1)
-        {
-            for (int col = 0; col < n-1; col++)
-            {
-                cout << col+1<<" ";
-            }
-            
-        }
-        
         cout << endl;
         
     }
-    
+    for(int  i = 0 ; i<n;i++){
+        cout << i+1<<" ";
+    }
     return 0;
 }
--- StriverExtras/10RecursionL7.cpp
+++ StriverExtras/10RecursionL7.cpp
@@ -32,6 +32,32 @@ using namespace std;
 //     return ans;
 // }
 
+//Method2-->(Optimal Method)
+vector<long long> factorialNumbers(long long n) {
+
+    // Write Your Code Here
+
+    vector<long long> ans;
+
+    long long fact{1};
+
+    int i{2};
+
+    while(fact <= n){
+
+        ans.push_back(fact);
+
+        fact *= i;
+
+        i++;
+
+    }
+
+ 
+
+    return ans;
+
+}
 
 int main(){
     long long n;
