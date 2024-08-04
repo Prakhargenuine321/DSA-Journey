7bb4b2538872cba1793367c327faffed2d7f1e18
Prakhar <prakhargenuine9336@gmail.com>
2024-08-04 23:46:07 +0530

    Another Commit

--- Rough.cpp
+++ Rough.cpp
@@ -3,20 +3,11 @@ using namespace std;
 
 int main()
 {
-    vector<vector<int>> arr = {{1,2,3},{4,5,6},{7,8,9}};
-    int n = arr.size();
-    int ans[n][n];
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            ans[j][n - 1 - i] = arr[i][j];
-        }
-    }
-
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            cout << ans[i][j] << " ";
-        }
-        cout << endl;
+    vector<int> arr = {2 ,3 , 1 , 5 , 6};
+    sort(arr.begin() , arr.end());
+    reverse(arr.begin() , arr.end());
+    for(auto it : arr){
+        cout << it << " ";
     }
 
 
