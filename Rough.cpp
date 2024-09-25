288d2b0ffc5d7c473f08ed680eb5e12cc728690e
Prakhar <prakhargenuine9336@gmail.com>
2024-09-25 23:53:12 +0530

    Just Wanna Check Update

--- Rough.cpp
+++ Rough.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node{
+
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(){
+        this->data = 0;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+
 int main(){
 
     int num = 10;
@@ -10,6 +23,7 @@ int main(){
     p1 = &num1;
     cout << **p2 << endl;
     cout << p2 << endl;
+
 
 
     return 0;
