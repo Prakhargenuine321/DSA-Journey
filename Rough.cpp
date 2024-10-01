3d92fa7d4953b0b46ba3649cee7088740ad93c69
Prakhar <prakhargenuine9336@gmail.com>
2024-10-01 18:35:21 +0530

    Notes updated

--- Rough.cpp
+++ Rough.cpp
@@ -1,29 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Node{
-
-    int data;
-    Node* next;
-    Node* prev;
-
-    Node(){
-        this->data = 0;
-        this->next = nullptr;
-        this->prev = nullptr;
-    }
-};
 
 int main(){
-    int num2 = 303;
-    int num = 10;
-    int* p1 = &num;
-    int** p2 = &p1;
-    int num1 = 30;
-    p1 = &num1;
-    cout << **p2 << endl;
-    cout << p2 << endl;
-
+    cout << 1%2 << endl;
 
 
     return 0;
