217116eb2cab26422babf8d7b6f084d78181a643
Prakhar <prakhargenuine9336@gmail.com>
2024-10-11 18:16:28 +0530

    Problem list updated

--- Rough.cpp
+++ Rough.cpp
@@ -1,37 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class MyStack
-{
-private:
-    int arr[1000];
-    int top;
-public:
-    MyStack(){top=-1;}
-    int pop();
-    void push(int);
-    int topElem();
-};
-
-void MyStack ::push(int x) {
-    // Your Code
-    if(top < 1000){
-        top++;
-        arr[top] = x;
-    }
-}
-
-int MyStack:: topElem(){
-    return arr[top];
-}
-
 int main(){
 
-    MyStack* stack = new MyStack();
-    stack->push(10);
-    stack->push(20);
-    stack->push(30);
-    cout << stack->topElem();
+    string s = "prakhar";
+    cout << s.length() << endl;
 
     return 0;
 }
