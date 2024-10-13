e3b44f986bd494001e413ee9ecdd3cf2b72a3b18
Prakhar <prakhargenuine9336@gmail.com>
2024-10-13 22:19:15 +0530

    Problem list updated

---
+++ #1STL/6stackNotes.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Topic-->Infix, Postfix and Prefix.
+//Infix-->normally used in programming languages like c++, java etc.
+//Postfix-->Implemented in stack based calculators.
+//Prefix-->used in LISP(list processing) programming languages.
+
+int main(){
+
+
+
+    return 0;
+}
