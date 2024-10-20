256f6369f0e99b8f6d2c277b469bf65a1c401411
Prakhar <prakhargenuine9336@gmail.com>
2024-10-20 23:57:29 +0530

    Rough It!

--- Rough.cpp
+++ Rough.cpp
@@ -1,10 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool parseBoolExp(string expression){
+    stack<char> stk;
+
+    bool answer;
+
+    int size = expression.length();
+
+    for(int i = size - 1; i >= 0 ; i--){
+        if(expression[i] == 'f' || expression[i] == 't'){
+            stk.push(expression[i]);
+        }else if(expression[i] == '!' || expression[i] == '&' || expression[i] == '|'){
+            bool ans = 1;
+            if(stk.top() == 'f'){
+                ans = 0;
+            }
+
+            while(!stk.empty()){
+                bool top = 1;
+                if(stk.top() == 'f'){
+                    top = 0;
+                }
+                if(expression[i] == '|'){
+                    ans = ans | top;
+                    stk.pop();
+                }else if(expression[i] == '&'){
+                    ans = ans & top;
+                    stk.pop();
+                }else{
+                    ans = !top;
+                    stk.pop();
+                }
+            }
+            answer = ans;
+        }
+    }
+    return answer;
+}
+
 int main(){
 
-    string s = "prakhar";
-    cout << s.length() << endl;
+    bool answer = parseBoolExp("!(|t(&(f,t,f)))");
+
+    cout << answer;
 
     return 0;
 }
