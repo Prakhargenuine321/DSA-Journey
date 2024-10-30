c71606c299c02fef03a83d20e159db001d071da4
Prakhar <prakhargenuine9336@gmail.com>
2024-10-30 11:44:39 +0530

    Problem list updated

--- Rough.cpp
+++ Rough.cpp
@@ -1,49 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool parseBoolExp(string expression){
-    stack<char> stk;
-
-    bool answer;
-
-    int size = expression.length();
-
-    for(int i = size - 1; i >= 0 ; i--){
-        if(expression[i] == 'f' || expression[i] == 't'){
-            stk.push(expression[i]);
-        }else if(expression[i] == '!' || expression[i] == '&' || expression[i] == '|'){
-            bool ans = 1;
-            if(stk.top() == 'f'){
-                ans = 0;
-            }
-
-            while(!stk.empty()){
-                bool top = 1;
-                if(stk.top() == 'f'){
-                    top = 0;
-                }
-                if(expression[i] == '|'){
-                    ans = ans | top;
-                    stk.pop();
-                }else if(expression[i] == '&'){
-                    ans = ans & top;
-                    stk.pop();
-                }else{
-                    ans = !top;
-                    stk.pop();
-                }
-            }
-            answer = ans;
-        }
-    }
-    return answer;
-}
-
 int main(){
 
-    bool answer = parseBoolExp("!(|t(&(f,t,f)))");
+    stack<int> stk;
 
-    cout << answer;
+    stk.push(10);
+    stk.push(20);
+    stk.pop();
+
+    int a = 0 , b = 0;
+
+    cout << stk.size() << endl;
+
+    cout << a << "--" << b << endl; 
 
     return 0;
 }
